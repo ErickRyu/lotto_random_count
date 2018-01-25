962e99120d34b5ef4ea666871b73c83b80d5ab36
ErickRyu <ErickRyu>
2018-01-26 01:40:58 +0900

    Refactoring: rename, change logic

--- lotto.py
+++ lotto.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 from numpy import genfromtxt
 
-origin = range(1, 46)
+numSet = range(1, 46)
 data = []
 
 def read_data():
@@ -10,23 +10,22 @@ def read_data():
     data = genfromtxt('lodata.csv', delimiter=',', dtype=int)
 
 
-def logic():
-    nums = origin[:]
+def getRandCount(toCompare):
+    nums = numSet[:]
     popped = []
-    toCompare = [3, 8, 19, 27, 30, 41]
     count = 0
-    while(not np.array_equal(data[0], popped)):
-        nums = origin[:]
+    while(not np.array_equal(toCompare, popped)):
+        nums = numSet[:]
         popped = []
         for i in range(0, 6):
             popped.insert(0, nums.pop(random.randint(0, len(nums)-1)))
         popped.sort()
         count += 1
 
-    print popped
-    print count
+    return count
 
 
 if __name__ == '__main__':
     read_data()
-    logic()
+    count = getRandCount(data[0])
+    print count
